c37ef0bf947719439c2366be77a392782d8920ec
yuhao <83017426@qq.com>
2019-03-17 21:23:03 +0800

    let VimBuildCmd.sh run

--- vimTools/VimBuildCmd.sh
+++ vimTools/VimBuildCmd.sh
@@ -4,4 +4,7 @@ sudo apt-get install libxt-dev
 sudo apt-get install libgtk2.0-dev
 sudo apt-get install libgnome2-dev
 cd ./vim
-./configure --disable-selinux --enable-perlinterp=yes --enable-pythoninterp --enable-rubyinterp=yes --enable-cscope --enable-gui=auto --with-features=huge --enable-multibyte --enable-xim --with-x --with-gnome --with-compiled="goodfish" --prefix=/usr/local/vim8
+./configure --disable-selinux --enable-perlinterp=yes --enable-pythoninterp --enable-rubyinterp=yes --enable-cscope --enable-gui=auto --with-features=huge --enable-multibyte --enable-xim --with-x --with-gnome --with-compiledby="goodfish" --prefix=/usr/local/vim8
+
+make
+sudo make install
