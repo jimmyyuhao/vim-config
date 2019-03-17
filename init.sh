661d2374a1188ffcc59c19fe0a2016933747f73e
yuhao <83017426@qq.com>
2019-03-17 16:54:56 +0800

    add top init.sh

---
+++ init.sh
@@ -0,0 +1,22 @@
+ln -s ./mytools/dotfiles/vimrc/.vimrc  ~/.vimrc
+ln -s ./mytools/vimTools/.vim/  ~/.vim
+ln -s ./mytools/dotfiles/xterm/.Xdefaults ~/.Xdefaults
+ln -s ./mytools/dotfiles/xterm/.Xresources ~/.Xresources
+ln -s ./mytools/zshTools/.zshrc ~/.zshrc
+
+sudo apt-get install ttf-wqy-zenhei
+sudo apt-get install xterm
+
+sudo apt-get install cppcheck
+sudo apt-get install clang
+sudo apt-get install astyle
+sudo apt-get install vim
+sudo apt-get install gdb
+sudo atp-get install bear
+sudo apt-get install gnu-global //gtags
+sudo apt-get install universal_ctags
+sudo apt-get install the_silver_searcher //ag
+sudo apt-get install tmux
+
+
+restart
