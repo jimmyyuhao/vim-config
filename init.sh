d549e226a21d1715cc1d4f7dce9653bb1f9ce470
yuhao <83017426@qq.com>
2019-03-17 18:15:45 +0800

    add tmux config file

--- init.sh
+++ init.sh
@@ -1,8 +1,10 @@
-ln -s ./mytools/dotfiles/vimrc/.vimrc  ~/.vimrc
-ln -s ./mytools/vimTools/.vim/  ~/.vim
-ln -s ./mytools/dotfiles/xterm/.Xdefaults ~/.Xdefaults
-ln -s ./mytools/dotfiles/xterm/.Xresources ~/.Xresources
-ln -s ./mytools/zshTools/.zshrc ~/.zshrc
+ln -s ~/mytools/dotfiles/bash/.bashrc ~/.bashrc
+ln -s ~/mytools/dotfiles/vimrc/.vimrc  ~/.vimrc
+ln -s ~/mytools/dotfiles/tmux/.tmux.conf ~/.tmux.conf
+ln -s ~/mytools/dotfiles/xterm/.Xdefaults ~/.Xdefaults
+ln -s ~/mytools/dotfiles/xterm/.Xresources ~/.Xresources
+ln -s ~/mytools/vimTools/.vim/  ~/.vim
+ln -s ~/mytools/zshTools/.zshrc ~/.zshrc
 
 sudo apt-get install ttf-wqy-zenhei
 sudo apt-get install xterm
