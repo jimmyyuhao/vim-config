70294c5c7299721fb749a872ae2941b92f0a8935
yuhao <83017426@qq.com>
2019-03-17 22:11:45 +0800

    add Plug.vim manually && make VimBuildCmd auto

--- vimTools/VimBuildCmd.sh
+++ vimTools/VimBuildCmd.sh
@@ -4,7 +4,9 @@ sudo apt-get install libxt-dev
 sudo apt-get install libgtk2.0-dev
 sudo apt-get install libgnome2-dev
 cd ./vim
-./configure --disable-selinux --enable-perlinterp=yes --enable-pythoninterp --enable-rubyinterp=yes --enable-cscope --enable-gui=auto --with-features=huge --enable-multibyte --enable-xim --with-x --with-gnome --with-compiledby="goodfish" --prefix=/usr/local/vim8
+./configure --disable-selinux --enable-perlinterp=yes --enable-pythoninterp --enable-rubyinterp=yes --enable-cscope --enable-gui=auto --with-features=huge --enable-multibyte --enable-xim --with-x --with-gnome --with-compiledby="goodfish" --prefix=/usr/local/bin
 
 make
 sudo make install
+sudo mv /usr/local/bin/vim /usr/local/bin/vim_bak
+sudo ln -s  /usr/local/vim8/bin/vim /usr/local/bin/vim
