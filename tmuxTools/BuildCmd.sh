0c3f283b1c68e4f03962b6d97047cc5f95a3bc95
yuhao <83017426@qq.com>
2019-03-24 11:54:14 +0800

    BuildCmd update

---
+++ tmuxTools/BuildCmd.sh
@@ -0,0 +1,29 @@
+#!/bin/bash/
+#https://github.com/tmux/tmux/wiki
+
+#tmux depends on libevent and ncurses.
+#http://libevent.org/
+#https://invisible-island.net/ncurses/#download_ncurses
+
+# tar -xvf ./libevent-2.1.8-stable.tar.gz
+# cd ./libevent-2.1.8-stable
+# ./configure
+# make
+# sudo make install
+#
+# tar -xvf ./ncurses.tar.gz
+# cd ./ncurses-6.1/
+# ./configure
+# make
+# sudo make install
+
+
+cd ./tmux/
+sudo apt-get remove tmux
+sudo apt-get install libevent-dev
+sudo apt-get install libncurses5-dev
+sudo apt-get install xsel
+sudo apt-get install xclip
+sh autogen.sh
+./configure && make
+sudo make install
--- vimTools/ThirdPartyTools/BuildCmd.sh
+++ vimTools/ThirdPartyTools/BuildCmd.sh
@@ -1,13 +1,58 @@
 #!/bin/bash
+CURDIR=$(cd "$(dirname "$0")";pwd)
+echo in $CURDIR
 
 #BEAR install Cmd
-cd ./Bear
+cd $CURDIR/Bear
 mkdir ./build
 cd build
 cmake ./../bear
 make all
-sudo make install # to install
+#sudo make install # to install
 
+#connect install Cmd
+cd $CURDIR/connect/gotoh-connect-2e080df38b35/
+make clean
+make
+#sudo make install # to install
 
-#
+#cppcheck install Cmd
+cd $CURDIR/cppcheck/
+mkdir ./build
+cd build
+cmake ./../
+make
+#sudo make install # to install
 
+#ctags install Cmd
+cd $CURDIR/ctags/
+./autogen.sh
+make
+#sudo make install # to install
+
+#gdb install Cmd
+cd $CURDIR/gdb/
+tar -xvf ./gdb-8.2.tar.xz
+cd ./gdb-8.2/
+./configure
+make
+#sudo make install # to install
+
+#gnu-global install Cmd
+cd $CURDIR/gnu-global/source-linux
+tar -xvf ./global-6.6.3.tar.gz
+cd ./global-6.6.3
+./configure
+make
+#sudo make install # to install
+
+#pip install Cmd
+cd $CURDIR/pip
+python get-pip.py
+pip install Pygments
+
+#the_silver_search install Cmd
+cd $CURDIR/the_silver_search/
+./autogen.sh
+make
+#sudo make install # to install
