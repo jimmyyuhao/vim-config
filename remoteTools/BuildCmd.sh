4fc9217c7bdcda768928ae18575a086b3c1765e1
yuhao <83017426@qq.com>
2019-03-24 11:58:26 +0800

    remote BuildCmd update

--- remoteTools/BuildCmd.sh
+++ remoteTools/BuildCmd.sh
@@ -4,8 +4,8 @@ sudo apt-get install tightvncserver xrdp
 sudo apt-get install xfce4-session
 sudo apt-get install libglib2.0-bin
 sudo apt-get install libpulse-dev
-#sudo apt-get install gedit
-#gedit /etc/xrdp/startwm.sh
+sudo apt-get install gedit
+gedit /etc/xrdp/startwm.sh
 touch ~/.xsession
 echo xfce4-session >~/.xsession
 sudo rm /var/run/xrdp/xrdp.pid 
