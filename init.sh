d1d49dfd8c481c256072d29bd8328acefea2a50f
jimmy.yuhao <31171986+jimmyyuhao@users.noreply.github.com>
2019-04-02 14:19:00 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,11 +1,12 @@
 #!/bin/bash/
 ln -s ~/mytools/dotfiles/bash/.bashrc ~/.bashrc
+ln -s ~/mytools/dotfiles/zsh/.zshrc ~/.zshrc
 ln -s ~/mytools/dotfiles/vimrc/.vimrc  ~/.vimrc
 ln -s ~/mytools/dotfiles/tmux/.tmux.conf ~/.tmux.conf
 ln -s ~/mytools/dotfiles/xterm/.Xdefaults ~/.Xdefaults
 ln -s ~/mytools/dotfiles/xterm/.Xresources ~/.Xresources
 ln -s ~/mytools/vimTools/.vim/  ~/.vim
-ln -s ~/mytools/zshTools/.zshrc ~/.zshrc
+
 
 git submodule init
 git submodule update
