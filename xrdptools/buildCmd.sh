b65163566ec87d0660d2edc32935b1a3f09b9efe
jimmy.yuhao <31171986+jimmyyuhao@users.noreply.github.com>
2019-06-18 19:05:20 +0800

    Update buildCmd.sh

--- xrdptools/buildCmd.sh
+++ xrdptools/buildCmd.sh
@@ -1,1 +1,26 @@
 
+#1. 安装xRDP及vncserver
+
+sudo apt-get install xrdp
+sudo apt-get install vnc4server tightvncserver 
+#2. 安装Xfce桌面环境
+
+sudo apt-get install xubuntu-desktop 
+#3. 设置xRDP
+
+echo xfce4-session >~/.xsession 
+#4. 设置配置文件
+
+sudo vim.tiny /etc/xrdp/startwm.sh
+#在. /etc/X11/Xsession 前一行插入
+#xfce4-session 
+
+#5. 重启 xrdp
+sudo service xrdp restart
+
+#6  vim.tiny gedit /etc/xrdp/sesman.ini
+#在末尾加上：
+#param8=-SecurityTypes
+#param9=None
+
+#7. 最后测试远程链接
