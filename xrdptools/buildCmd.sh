b0af1bc67b47db67efcb5815f274fff464a38dbf
jimmy.yuhao <31171986+jimmyyuhao@users.noreply.github.com>
2019-06-19 08:43:06 +0800

    Update buildCmd.sh

--- xrdptools/buildCmd.sh
+++ xrdptools/buildCmd.sh
@@ -15,12 +15,18 @@ sudo vim.tiny /etc/xrdp/startwm.sh
 #在. /etc/X11/Xsession 前一行插入
 #xfce4-session 
 
-#5. 重启 xrdp
-sudo service xrdp restart
 
-#6  vim.tiny gedit /etc/xrdp/sesman.ini
+#6  vim.tiny /etc/xrdp/sesman.ini
+#中间修改
+#MaxSessions=1000
 #在末尾加上：
 #param8=-SecurityTypes
 #param9=None
 
-#7. 最后测试远程链接
+#5. 重启 xrdp
+sudo service xrdp restart
+
+#7 在远程桌面中设置，打开菜单->设置->窗口管理器，或者在终端中输入xfwm4-settings打开（xfwm4就是xfce4 window manger的缩写）
+#选择键盘，可以看到窗口快捷键中动作一列有“切换同一应用程序的窗口”选项，将该选项的快捷键清除后关闭窗口即可。
+#到此，Tab键自动补全功能配置完成。
+#8. 最后测试远程链接
