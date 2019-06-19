ba7246cad1aa2038b4d05bdb1e6725ef556b3ec9
jimmy.yuhao <31171986+jimmyyuhao@users.noreply.github.com>
2019-06-19 16:13:45 +0800

    Update VimBuildCmd.sh

--- vimTools/VimBuildCmd.sh
+++ vimTools/VimBuildCmd.sh
@@ -4,7 +4,7 @@ sudo apt-get install libxt-dev
 sudo apt-get install libgtk2.0-dev
 sudo apt-get install libgnome2-dev
 cd ./vim
-./configure --disable-selinux --enable-perlinterp=yes --enable-pythoninterp --enable-rubyinterp=yes --enable-cscope --enable-gui=auto --with-features=huge --enable-multibyte --enable-xim --with-x --with-gnome --with-compiledby="goodfish" --prefix=/usr/local/bin
+./configure --disable-selinux --enable-perlinterp=yes --enable-pythoninterp --enable-rubyinterp=yes --enable-cscope --enable-gui=auto --with-features=huge --enable-multibyte --enable-xim --with-x --with-gnome --with-compiledby="goodfish" --prefix=/usr/local/
 
 make
 sudo make install
