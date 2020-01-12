2df7b731d44e724f33080722d379fb2e7c4a2651
jimmy.yuhao <83017426@qq.com>
2020-01-12 19:26:33 +0800

    modify setup script

--- zshTools/BuildCmd.sh
+++ zshTools/BuildCmd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 CURDIR=$(cd "$(dirname "$0")";pwd)
 echo in $CURDIR
-cp $CURDIR/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+cp $CURDIR/oh-my-zsh/templates/zshrc.zsh-template $CURDIR/.zshrc
 rm -rf ~/.oh-my-zsh
 ln -s $CURDIR/oh-my-zsh ~/.oh-my-zsh
