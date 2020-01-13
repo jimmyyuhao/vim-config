da7cbd4811de567e0ec55cb9b14b1f7b88a2eaa0
jimmy.yuhao <83017426@qq.com>
2020-01-13 21:33:55 +0800

    update tmux

--- vimTools/VimBuildCmd.sh
+++ vimTools/VimBuildCmd.sh
@@ -10,3 +10,6 @@ make
 sudo make install
 sudo mv /usr/local/bin/vim /usr/local/bin/vim_bak
 sudo ln -s  /usr/local/vim8/bin/vim /usr/local/bin/vim
+
+./configure --disable-selinux --enable-perlinterp=yes --enable-pythoninterp=yes  --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu/ --enable-python3interp=yes  --with-python3-config-dir=/usr/lib/python3.6/config-3.6m-x86_64-linux-gnu/ --enable-rubyinterp=yes --enable-cscope --enable-gui=auto --with-features=huge --enable-multibyte --enable-xim --with-x --with-gnome --with-compiledby="goodfish" --prefix=/usr/local/
+
