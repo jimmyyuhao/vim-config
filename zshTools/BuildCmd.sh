87f21559534d4fac28bf2fff8712b61fba5201d6
jimmy.yuhao <83017426@qq.com>
2020-01-19 20:29:55 +0800

    fix zsh

--- zshTools/BuildCmd.sh
+++ zshTools/BuildCmd.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 CURDIR=$(cd "$(dirname "$0")";pwd)
 echo in $CURDIR
+#git clone https://github.com/robbyrussell/oh-my-zsh.git ./oh-my-zsh
 cp $CURDIR/oh-my-zsh/templates/zshrc.zsh-template $CURDIR/.zshrc
 rm -rf ~/.oh-my-zsh
 ln -s $CURDIR/oh-my-zsh ~/.oh-my-zsh
+#if unsafe message print after zsh start
+# cd ./oh-my-zsh/ && compaudit | xargs chmod g-w,o-w
